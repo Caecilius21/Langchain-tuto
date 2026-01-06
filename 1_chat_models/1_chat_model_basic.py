e0f7722c5073c1b3a94573a705a70eadccf92563
Aksel Maud <ayoub.bouhachmoud@gmail.com>
2026-01-06 16:58:33 +0100

    Initial commit

---
+++ 1_chat_models/1_chat_model_basic.py
@@ -0,0 +1,18 @@
+# Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
+# OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
+
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatMistralAI(model="mistral-large-latest")
+
+# Invoke the model with a message
+result = model.invoke("What is 81 divided by 9?")
+print("Full result:")
+print(result)
+print("Content only:")
+print(result.content)
---
+++ 1_chat_models/2_chat_model_basic_conversation.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_mistralai import ChatMistralAI
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatMistralAI(model="mistral-large-latest")
+
+# SystemMessage:
+#   Message for priming AI behavior, usually passed in as the first of a sequence of input messages.
+# HumanMessage:
+#   Message from a human to the AI model.
+messages = [
+    SystemMessage(content="Solve the following math problems"),
+    HumanMessage(content="What is 81 divided by 9?"),
+]
+
+# Invoke the model with messages
+result = model.invoke(messages)
+print(f"Answer from AI: {result.content}")
+
+
+# AIMessage:
+#   Message from an AI.
+messages = [
+    SystemMessage(content="Solve the following math problems"),
+    HumanMessage(content="What is 81 divided by 9?"),
+    AIMessage(content="81 divided by 9 is 9."),
+    HumanMessage(content="What is 10 times 5?"),
+]
+
+# Invoke the model with messages
+result = model.invoke(messages)
+print(f"Answer from AI: {result.content}")
